5ef8b314085c49aeb9071e8fb54767831ad148aa
Peien Xie <peienxieit@gmail.com>
2021-06-15 10:36:09 +0800

    add configparse for server settings

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,59 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"strconv"
 	"tcprelay"
 	"tcprelay/relaytarget"
+
+	"github.com/alyu/configparser"
 )
 
-var (
-	MIDDLE_SERVER_PORT = 8088
-)
+type Config struct {
+	MiddleServerPort int
+	TargetServerIp   string
+	TargetServerPort int
+}
+
+func (c Config) String() string {
+	return fmt.Sprintf("MiddleServerPort: %d\nTargetServerIp: %s\nTargetServerPort: %d\n",
+		c.MiddleServerPort,
+		c.TargetServerIp,
+		c.TargetServerPort,
+	)
+}
+
+var appConfig Config
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	config, err := configparser.Read("config.ini")
+	checkErr(err)
+	section, err := config.Section("CONFIG")
+	checkErr(err)
+
+	appConfig.MiddleServerPort, err = strconv.Atoi(section.ValueOf("MiddleServerPort"))
+	checkErr(err)
+
+	appConfig.TargetServerIp = section.ValueOf("TargetServerIp")
+
+	appConfig.TargetServerPort, err = strconv.Atoi(section.ValueOf("TargetServerPort"))
+	checkErr(err)
+
+	fmt.Printf("using configuration below:\n%s\n", appConfig)
+}
 
 func main() {
 	s := tcprelay.NewTcpRelayServer(
-		MIDDLE_SERVER_PORT,
-		// relaytarget.NewRelayTarget("18.235.124.214:80"),
-		relaytarget.NewListenableRelayTarget(8089),
+		appConfig.MiddleServerPort,
+		relaytarget.NewRelayTarget(fmt.Sprintf("%s:%d", appConfig.TargetServerIp, appConfig.TargetServerPort)),
+		// relaytarget.NewListenableRelayTarget(8089),
 	)
 	s.Listen()
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module tcprelay
 
 go 1.13
 
-require github.com/google/uuid v1.2.0
+require (
+	github.com/alyu/configparser v0.0.0-20191103060215-744e9a66e7bc // indirect
+	github.com/google/uuid v1.2.0
+)
